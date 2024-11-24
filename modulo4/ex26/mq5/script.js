df4498eb10a2585fed12eb94674838fa4737a1f2
Gabriel Luiz Fonseca <bielnfonseca@gmail.com>
2024-11-24 18:39:44 -0300

    projeto atualizado

--- modulo4/ex26/mq5/script.js
+++ modulo4/ex26/mq5/script.js
@@ -1,3 +1,12 @@
+function mdtamanho(){
+    if(window.innerWidth >= 768){
+        home.style.display = 'block'
+    }else{
+        home.style.display = 'none'
+    }
+}
+
+
 function clickmenu(){
     if (home.style.display == 'block'){
         home.style.display = 'none'
